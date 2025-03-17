47b3ee825d0cab4848e8e35804d6fdd00ffad6bb
Reece Williams <reecepbcups@gmail.com>
2025-03-17 12:11:48 -0500

    comment out test

--- x/wasm/keeper/wasmtesting/store.go
+++ x/wasm/keeper/wasmtesting/store.go
@@ -1,6 +1,8 @@
 package wasmtesting
 
 import (
+	"io"
+
 	storetypes "cosmossdk.io/store/types"
 )
 
@@ -18,6 +20,77 @@ func (m *MockCommitMultiStore) CacheMultiStore() storetypes.CacheMultiStore {
 type mockCMS struct {
 	storetypes.CommitMultiStore
 	committed *bool
+}
+
+// CacheMultiStore implements types.CacheMultiStore.
+// Subtle: this method shadows the method (CommitMultiStore).CacheMultiStore of mockCMS.CommitMultiStore.
+func (m *mockCMS) CacheMultiStore() storetypes.CacheMultiStore {
+	panic("unimplemented")
+}
+
+// CacheMultiStoreWithVersion implements types.CacheMultiStore.
+// Subtle: this method shadows the method (CommitMultiStore).CacheMultiStoreWithVersion of mockCMS.CommitMultiStore.
+func (m *mockCMS) CacheMultiStoreWithVersion(version int64) (storetypes.CacheMultiStore, error) {
+	panic("unimplemented")
+}
+
+// CacheWrap implements types.CacheMultiStore.
+// Subtle: this method shadows the method (CommitMultiStore).CacheWrap of mockCMS.CommitMultiStore.
+func (m *mockCMS) CacheWrap() storetypes.CacheWrap {
+	panic("unimplemented")
+}
+
+// CacheWrapWithTrace implements types.CacheMultiStore.
+// Subtle: this method shadows the method (CommitMultiStore).CacheWrapWithTrace of mockCMS.CommitMultiStore.
+func (m *mockCMS) CacheWrapWithTrace(w io.Writer, tc storetypes.TraceContext) storetypes.CacheWrap {
+	panic("unimplemented")
+}
+
+// Copy implements types.CacheMultiStore.
+func (m *mockCMS) Copy() storetypes.CacheMultiStore {
+	panic("unimplemented")
+}
+
+// GetKVStore implements types.CacheMultiStore.
+// Subtle: this method shadows the method (CommitMultiStore).GetKVStore of mockCMS.CommitMultiStore.
+func (m *mockCMS) GetKVStore(storetypes.StoreKey) storetypes.KVStore {
+	panic("unimplemented")
+}
+
+// GetStore implements types.CacheMultiStore.
+// Subtle: this method shadows the method (CommitMultiStore).GetStore of mockCMS.CommitMultiStore.
+func (m *mockCMS) GetStore(storetypes.StoreKey) storetypes.Store {
+	panic("unimplemented")
+}
+
+// GetStoreType implements types.CacheMultiStore.
+// Subtle: this method shadows the method (CommitMultiStore).GetStoreType of mockCMS.CommitMultiStore.
+func (m *mockCMS) GetStoreType() storetypes.StoreType {
+	panic("unimplemented")
+}
+
+// LatestVersion implements types.CacheMultiStore.
+// Subtle: this method shadows the method (CommitMultiStore).LatestVersion of mockCMS.CommitMultiStore.
+func (m *mockCMS) LatestVersion() int64 {
+	panic("unimplemented")
+}
+
+// SetTracer implements types.CacheMultiStore.
+// Subtle: this method shadows the method (CommitMultiStore).SetTracer of mockCMS.CommitMultiStore.
+func (m *mockCMS) SetTracer(w io.Writer) storetypes.MultiStore {
+	panic("unimplemented")
+}
+
+// SetTracingContext implements types.CacheMultiStore.
+// Subtle: this method shadows the method (CommitMultiStore).SetTracingContext of mockCMS.CommitMultiStore.
+func (m *mockCMS) SetTracingContext(storetypes.TraceContext) storetypes.MultiStore {
+	panic("unimplemented")
+}
+
+// TracingEnabled implements types.CacheMultiStore.
+// Subtle: this method shadows the method (CommitMultiStore).TracingEnabled of mockCMS.CommitMultiStore.
+func (m *mockCMS) TracingEnabled() bool {
+	panic("unimplemented")
 }
 
 func (m *mockCMS) Write() {
